2fa1a105a5cca2c78f8540d07bed8103497fb4fc
marion419 <daynvi@gmail.com>
2024-12-23 17:34:16 +0900

    feat: Most popular movie zone

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -3,6 +3,41 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // https://picsum.photos/200/300
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          // 가장 인기있는 영화
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              width: double.infinity,
+              height: 500,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: const DecorationImage(
+                  image: AssetImage('assets/images/sample_poster.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+
+          const Text('현재 상영중'),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) {
+                return Text('data');
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }

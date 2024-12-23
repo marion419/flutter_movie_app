c3270afb599c30f2743b220da8a67383ebb34f5b
marion419 <daynvi@gmail.com>
2024-12-23 17:44:16 +0900

    feat: horizontal listview

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -24,18 +24,35 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
-
+          // 현재 상영중인 영화 행
           const Text('현재 상영중'),
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (BuildContext context, int index) {
-                return Text('data');
-              },
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: SizedBox(
+              width: double.infinity,
+              height: 180,
+              child: ListView.separated(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                separatorBuilder: (context, index) {
+                  return const SizedBox(width: 8);
+                },
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    width: 140,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blueAccent,
+                      image: const DecorationImage(
+                          image: AssetImage('assets/images/sample_poster.jpg'),
+                          fit: BoxFit.cover),
+                    ),
+                  );
+                },
+              ),
             ),
-          )
+          ),
         ],
       ),
     );

9b0f43ccb8bb6965cc5b9be094bbd56a3e9d96b5
marion419 <daynvi@gmail.com>
2024-12-26 17:30:04 +0900

    add: movie.dart

---
+++ lib/domain/entities/movie_detail.dart
@@ -0,0 +1,31 @@
+class Movie {
+  String title; // 제목
+  String overview; // 설명
+  String posterPath; // 포스터 URL
+  String releaseDate; // 개봉일
+  String tagline; // 태그라인
+  double popularity; // 인기 점수
+  double voteAverage; // 평점
+  int voteCount; // 평점 투표수
+  int budget; // 예산
+  int revenue; // 수익
+  int runtime; // 러닝타임
+  var genres; // 장르 배열
+  var productionCompanies; // 배급사 배열
+
+  Movie(
+    this.title,
+    this.overview,
+    this.posterPath,
+    this.releaseDate,
+    this.tagline,
+    this.popularity,
+    this.voteAverage,
+    this.voteCount,
+    this.budget,
+    this.revenue,
+    this.runtime,
+    this.genres,
+    this.productionCompanies,
+  );
+}

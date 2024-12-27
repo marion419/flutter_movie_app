2b7cdb7e47625ce609fff2afc40529a9fa7d354e
marion419 <daynvi@gmail.com>
2024-12-27 17:48:29 +0900

    add: MovieRepositoryImpl

--- lib/domain/repositories/movie_repository_impl.dart
+++ lib/domain/repositories/movie_repository_impl.dart
@@ -1,34 +1,24 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_movie_app/data/data_resources/movie_data_source.dart';
 import 'package:flutter_movie_app/domain/entities/movie.dart';
 import 'package:flutter_movie_app/domain/repositories/movie_repository.dart';
 
 class MovieRepositoryImpl implements MovieRepository {
+  MovieRepositoryImpl(this._movieDataSource);
+
+  final MovieDataSource _movieDataSource;
+
   @override
   Future<List<Movie>> findMovies(String path) async {
-    final Dio _client = Dio(BaseOptions(
-      validateStatus: (status) => true,
-    ));
-
-    final response = await _client.get(path,
-        queryParameters: <String, dynamic>{'page': 1, 'language': 'en-US'},
-        options: Options(headers: <String, dynamic>{
-          'Authorization': const String.fromEnvironment('Authorization'),
-          'accept': const String.fromEnvironment('accept'),
-        }));
-
-    try {
-      if (response.statusCode == 200) {
-        final dynamic movies = response.data['results'];
-        final Iterable<Movie> iterable =
-            List<dynamic>.from(movies).map<Movie>((dynamic e) {
-          return movies.fromJson(e);
-        });
-        return iterable.toList();
-      }
-      return <Movie>[];
-    } catch (e) {
-      print(e);
-      return <Movie>[];
-    }
+    final result = await _movieDataSource.findMovies(path);
+    return result
+        .map(
+          (e) => Movie(
+            e.title, // 제목
+            e.posterPath, // 포스터 링크
+            e.id, // 영화 아이디
+          ),
+        )
+        .toList();
   }
 }

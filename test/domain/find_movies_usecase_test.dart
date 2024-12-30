f80562f0df4c39c9780a88a107931783845e3b69
marion419 <daynvi@gmail.com>
2024-12-30 15:24:18 +0900

    test: FindMoviesUsecase

---
+++ test/domain/find_movies_usecase_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_movie_app/data/data_resources/movie_data_source_impl.dart';
+import 'package:flutter_movie_app/domain/repositories/movie_repository_impl.dart';
+import 'package:flutter_movie_app/domain/usecases/find_movies_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('find movies usecase test', () async {
+    // 데이터 소스
+    MovieDataSourceImpl _movieDataSource = MovieDataSourceImpl();
+    // 데이터소스 -> 레포지토리
+    MovieRepositoryImpl _movieRepository =
+        MovieRepositoryImpl(_movieDataSource);
+    // 레포지토리 -> usecase
+    final _findMoviesUsecase = FindMoviesUsecase(_movieRepository);
+    // usecase의 excute 함수 실행
+    final result = await _findMoviesUsecase
+        .excute('https://api.themoviedb.org/3/movie/now_playing');
+    expect(result.isEmpty, false);
+  });
+}

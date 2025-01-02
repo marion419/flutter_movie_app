951dbfa1b263e87933bff5efce93a475cf3e4aa5
marion419 <daynvi@gmail.com>
2025-01-02 09:27:02 +0900

    test: FindDetailUsecase

---
+++ test/domain/find_detail_usecase_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_movie_app/data/data_resources/movie_detail_data_source_impl.dart';
+import 'package:flutter_movie_app/domain/repositories/movie_detail_repository_impl.dart';
+import 'package:flutter_movie_app/domain/usecases/find_detail_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('find detail usecase test', () async {
+    MovieDetailDataSourceImpl _movieDetailDataSource =
+        MovieDetailDataSourceImpl();
+    MovieDetailRepositoryImpl _movieDetailRepository =
+        MovieDetailRepositoryImpl(_movieDetailDataSource);
+
+    final _findDetailUsecase = FindDetailUsecase(_movieDetailRepository);
+    final result = await _findDetailUsecase.excute(845781);
+    expect(result == null, false);
+    if (result != null) {
+      print(result.genres[0].name);
+    } else {
+      print('failed');
+    }
+  });
+}

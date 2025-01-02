1e1f60f76b41791afc04e99786367e137df53b0f
marion419 <daynvi@gmail.com>
2025-01-02 09:26:46 +0900

    fix: Change variable type of MovieDetailRepositoryImpl - findDetail

--- lib/domain/repositories/movie_detail_repository_impl.dart
+++ lib/domain/repositories/movie_detail_repository_impl.dart
@@ -17,12 +17,12 @@ class MovieDetailRepositoryImpl implements MovieDetailRepository {
           result.posterPath,
           result.releaseDate,
           result.tagline,
-          double.parse(result.popularity),
-          double.parse(result.voteAverage),
-          int.parse(result.voteCount),
-          int.parse(result.budget),
-          int.parse(result.revenue),
-          int.parse(result.runtime),
+          result.popularity,
+          result.voteAverage,
+          result.voteCount,
+          result.budget,
+          result.revenue,
+          result.runtime,
           result.genres,
           result.productionCompanies);
     }
